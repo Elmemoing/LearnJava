7f553bd9151b2a15a4b76abc19f650581a22831f
Guillermo Arguello Cordobe <74156373+Elmemoing@users.noreply.github.com>
2022-07-13 15:13:33 -0400

    Add files via upload

---
+++ PrimerProyecto/PrimerClase.java
@@ -0,0 +1,17 @@
+
+/**
+ * Write a description of class PrimerClase here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class PrimerClase
+{ 
+    public static void main (String[] args){    
+        System.out.println("Hola Mundo");
+    }
+}
+
+
+
+
