5a48413f28b51960fe60c3737db292234fc268d8
Guillermo Arguello Cordobe <74156373+Elmemoing@users.noreply.github.com>
2022-07-20 15:13:08 -0400

    Add files via upload

---
+++ PrimerProyecto/Suma.java
@@ -0,0 +1,21 @@
+
+/**
+ * Write a description of class Suma here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Suma
+{ 
+    public static void main (String[] args){
+        int numero1 = 1;
+        int numero2 = 5;
+        
+        int suma = numero1 + numero2;
+        System.out.println(suma);
+    }
+}
+
+
+
+
